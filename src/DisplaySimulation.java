0595eddb5f1430bcbe00fc288c4037099a8e9a99
cocosar07 <cocosar07370@gmail.com>
2016-11-08 10:30:34 +0100

    Improve display simulation.

--- src/DisplaySimulation.java
+++ src/DisplaySimulation.java
@@ -14,10 +14,32 @@ public class DisplaySimulation extends JPanel
 	private Map map;
 	private List<Agent> agents;
 	
+	Image[] images;
+	
 	public DisplaySimulation(Map map, List<Agent> agents)
 	{
 		this.map = map;
 		this.agents = agents;
+		
+		this.images = new Image[11];
+		try
+		{
+			this.images[0] = ImageIO.read(new File("img/mur_haut_gauche.png"));
+			this.images[1] = ImageIO.read(new File("img/mur_bas_gauche.png"));
+			this.images[2] = ImageIO.read(new File("img/mur_gauche.png"));
+			this.images[3] = ImageIO.read(new File("img/mur_haut_droit.png"));
+			this.images[4] = ImageIO.read(new File("img/mur_bas_droit.png"));
+			this.images[5] = ImageIO.read(new File("img/mur_droit.png"));
+			this.images[6] = ImageIO.read(new File("img/mur_haut.png"));
+			this.images[7] = ImageIO.read(new File("img/mur_bas.png"));
+			this.images[8] = ImageIO.read(new File("img/apple2.png"));
+			this.images[9] = ImageIO.read(new File("img/case_vide.png"));
+			this.images[10] = ImageIO.read(new File("img/pacman.png"));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -42,53 +64,46 @@ public class DisplaySimulation extends JPanel
 
 				Image img = null;
 
-				try
+				if (i == 0)
 				{
-					if (i == 0)
-					{
-						// Dessiner mur de gauche
-						if (j == 0)
-							img = ImageIO.read(new File("img/mur_haut_gauche.png"));
-						else if (j == this.map.getHeight() + 1)
-							img = ImageIO.read(new File("img/mur_bas_gauche.png"));
-						else
-							img = ImageIO.read(new File("img/mur_gauche.png"));
-					}
-					else if (i == this.map.getWidth() + 1)
-					{
-						// Dessiner mur de droite
-						if (j == 0)
-							img = ImageIO.read(new File("img/mur_haut_droit.png"));
-						else if (j == this.map.getHeight() + 1)
-							img = ImageIO.read(new File("img/mur_bas_droit.png"));
-						else
-							img = ImageIO.read(new File("img/mur_droit.png"));
-					}
+					// Dessiner mur de gauche
+					if (j == 0)
+						img = images[0];
+					else if (j == this.map.getHeight() + 1)
+						img = images[1];
+					else
+						img = images[2];
+				}
+				else if (i == this.map.getWidth() + 1)
+				{
+					// Dessiner mur de droite
+					if (j == 0)
+						img = images[3];
+					else if (j == this.map.getHeight() + 1)
+						img = images[4];
+					else
+						img = images[5];
+				}
+				else
+				{
+					if (j == 0)
+						img = images[6];
+					else if (j == this.map.getHeight() + 1)
+						img = images[7];
 					else
 					{
-						if (j == 0)
-							img = ImageIO.read(new File("img/mur_haut.png"));
-						else if (j == this.map.getHeight() + 1)
-							img = ImageIO.read(new File("img/mur_bas.png"));
+						if (this.map.getPatchIndex(new Position(i - 1, j - 1)) >= 0)
+						{
+							img = this.images[8];
+						}
 						else
 						{
-							if (this.map.getPatchIndex(new Position(i - 1, j - 1)) >= 0)
-							{
-								img = ImageIO.read(new File("img/apple2.png"));
-							}
-							else
-							{
-								img = ImageIO.read(new File("img/case_vide.png"));
-							}
+							img = this.images[9];
 						}
 					}
+				}
 
-					g.drawImage(img, x, y, blocWidth, blocHeight, this);
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
+				g.drawImage(img, x, y, blocWidth, blocHeight, this);
 			}
 		}
 
@@ -96,17 +111,10 @@ public class DisplaySimulation extends JPanel
 		{
 			for (Agent agent : this.agents)
 			{
-				try
-				{
-					Image img = ImageIO.read(new File("img/pacman.png"));
-					int x = ((int) (agent.getPosition().getX() + 1)) * blocWidth;
-					int y = ((int) (agent.getPosition().getY() + 1)) * blocHeight;
-					g.drawImage(img, x, y, blocWidth, blocHeight, this);
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
+				Image img = images[10];
+				int x = ((int) (agent.getPosition().getX() + 1)) * blocWidth;
+				int y = ((int) (agent.getPosition().getY() + 1)) * blocHeight;
+				g.drawImage(img, x, y, blocWidth, blocHeight, this);
 			}
 		}
 	}
