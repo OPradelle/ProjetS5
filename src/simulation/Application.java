3858a42760e32e4cb820f8acdd9fec83b78f9dd0
laetitiabourdon <laetitia-b-38@hotmail.fr>
2016-12-15 10:31:25 +0100

    Suppression de la boucle pour les tests.

--- src/simulation/Application.java
+++ src/simulation/Application.java
@@ -35,58 +35,49 @@ public class Application
 	
 	public void startSimulation(SimulationParameters simulation)
 	{
-        List<Integer> results = new ArrayList<Integer>();
-        for (int k = 0; k < 50; k++)
+        Map map = new Map();
+        map.readFromFile(simulation.getFilePath());
+        List<Agent> agents = new ArrayList<Agent>();
+        for (int i = 0; i < simulation.getAgentNumber(); i++)
         {
-            Map map = new Map();
-            map.readFromFile(simulation.getFilePath());
-            List<Agent> agents = new ArrayList<Agent>();
-            for (int i = 0; i < simulation.getAgentNumber(); i++)
+            agents.add(new Agent());
+            agents.get(i).setAgentController(factory.factoryMethod(simulation.getMovementType()));
+            agents.get(i).setPosition(new Position(this.rand.nextInt(map.getWidth()), this.rand.nextInt(map.getHeight())));
+        }
+
+        this.guiManager.getSimulationWindow().startDisplaySimulation(new DisplaySimulation(map, agents));
+        this.guiManager.showSimulationWindow();
+
+        while (map.patchNumberLeft() > 0 && this.guiManager.getSimulationWindow().isVisible())
+        {
+            for (Agent agent : agents)
             {
-                agents.add(new Agent());
-                agents.get(i).setAgentController(factory.factoryMethod(simulation.getMovementType()));
-                agents.get(i).setPosition(new Position(this.rand.nextInt(map.getWidth()), this.rand.nextInt(map.getHeight())));
+                agent.update(map);
             }
 
-            this.guiManager.getSimulationWindow().startDisplaySimulation(new DisplaySimulation(map, agents));
-            this.guiManager.showSimulationWindow();
+            this.guiManager.getSimulationWindow().repaint();
 
-            while (map.patchNumberLeft() > 0 && this.guiManager.getSimulationWindow().isVisible())
+            try
             {
-                for (Agent agent : agents)
-                {
-                    agent.update(map);
-                }
+                Thread.sleep(simulation.getSleepTime());
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
 
-                this.guiManager.getSimulationWindow().repaint();
-
-                try
-                {
-                    Thread.sleep(simulation.getSleepTime());
-                }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
+        if (map.patchNumberLeft() == 0)
+        {
+            int numberOfRound = 0;
+            for (Agent agent : agents)
+            {
+                numberOfRound += agent.getAgentController().getNumberOfMoves();
             }
 
-            if (map.patchNumberLeft() == 0)
-            {
-                int numberOfRound = 0;
-                for (Agent agent : agents)
-                {
-                    numberOfRound += agent.getAgentController().getNumberOfMoves();
-                }
-
-                //this.guiManager.showResult(numberOfRound / agents.size());
-                results.add(numberOfRound);
-            }
+            this.guiManager.showResult(numberOfRound / agents.size());
         }
         
-        for (Integer integer : results)
-        {
-            System.out.println(integer);
-        }
-        //this.guiManager.hideimulationWindow();
+        this.guiManager.hideimulationWindow();
 	}
 }
